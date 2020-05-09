6a93576f4d918c213bcd9044c5e01fb0097dd046
Xiaolong Li <xiaolong.li@shopee.com>
2020-05-09 11:19:08 +0800

    hooks测试

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const a = 'lix'
+    const a = 'lix';
+    console.log(qqq);
     return (
         <div className="App">
             <header className="App-header">
