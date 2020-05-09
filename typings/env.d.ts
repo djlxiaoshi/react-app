d6f0e182135fab8ecb319b701c86f4e3ab3362f6
Xiaolong Li <xiaolong.li@shopee.com>
2020-05-09 18:53:38 +0800

    完成脚手架基本配置

---
+++ typings/env.d.ts
@@ -0,0 +1,21 @@
+/** @format */
+
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.module.css' {
+    const classes: {[key: string]: string};
+    export default classes;
+}
+
+declare module '*.module.scss' {
+    const classes: {[key: string]: string};
+    export default classes;
+}
+
+declare module '*.module.sass' {
+    const classes: {[key: string]: string};
+    export default classes;
+}
